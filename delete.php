df22e20ae74dc619054374fba2d4ad8365023504
Simon Schick <simonsimcity@gmail.com>
2011-11-23 15:02:43 +0100

    The bug https://bugs.php.net/bug.php?id=58943 has been fixed. A fallback to a raw-socket-connection is not neccessary anymore.

--- delete.php
+++ delete.php
@@ -11,19 +11,9 @@ if (isset($_GET['delete_item'])){
 
 	$config = parse_ini_file('config.ini.php', true);
 
-	/*
-	 * Because of an API change on Memcache's side, the PHP module's 'delete'
-	 * doesn't work (it hasn't been updated). I've used raw sockets instead.
-	 *
-	 * See http://pecl.php.net/bugs/bug.php?id=16927 for bug report.
-	 */
-
-	$fs = fsockopen($config['SERVER']['SERVER'], $config['SERVER']['PORT']);
-
-	fwrite($fs, 'delete ' . $_GET['delete_item'] . "\r\n");
-
-	fclose($fs);
-
+	$MEMCACHE = new Memcache();
+	$MEMCACHE->connect($config['SERVER']['SERVER'], $config['SERVER']['PORT']);
+	$MEMCACHE->delete($_GET['delete_item']);
 }
 
 header("Location: index.php");
